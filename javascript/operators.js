9c70c61e992c55160bb0e2276996ebfe1174635a
shubh <shubhamdixit863@gmail.com>
2023-11-05 20:26:04 +0530

    js operators

--- javascript/operators.js
+++ javascript/operators.js
@@ -59,3 +59,79 @@ foo4(91)
 // prints bizz when the number is divisible by 5
 // prints fizz buz when the number is divisible by both 3 and 5
 //prints buzzfiz when the number is either divisble by 3 or 5
+
+function Fizzbuzz(num){
+    if(num%3==0 && num %5==0){
+        console.log("Fizzbuzz")
+    }else if(num%3==0){
+        console.log("Fizz")
+
+    }
+    else if( num%5==0){
+        console.log("Buzz")
+
+    }
+    else if(num%3==0 || num%5==0){
+        console.log("Buzz Fizz")
+
+    }
+
+
+}
+
+// Greater than less than operator
+
+console.log("Result of greater than",8>7) ; // Greater than 8>7 is evaluated to a boolean value it can either be true or false
+console.log("Result of lesser than",8<7) ; //Less than 8<7 is evaluated to a boolean value it can either be true or false
+// > ,< ,= ,== ,&& ,|| ,% ,+ ,-  , /, * , != (not equal to operator)
+
+// dividend (quotient)
+console.log("quotient",5/2) ; // it returns 2.5 ,the quotient
+
+// multiplication operator
+
+console.log("Multiplication", 4 *5 );
+
+console.log("IS",4!=3); // return true
+console.log("IS",4!=4); // return false
+
+
+//  ---> Write a function that checks whether the number is negative ,positive ,or zero 
+// if its negative --print negative number
+// if its positive -->print positibe number
+// if its zero ---> print zero number
+
+/**
+ * "Write a JavaScript function that takes two 
+ * numbers as input and print out the string
+ * containing their sum, difference, 
+ * product, quotient, and whether the 
+ * first number is greater than, less than, or
+ *  equal to the second number.
+ *  Ensure to use arithmetic, comparison, and logical operators in your solution."
+ */
+
+function foo47(num1,num2){
+    let sum=num1+num2;
+    let difference=num1-num2;
+    let multiplication=num1*num2;
+    let division=num1/num2;
+    if(num1>num2){
+        console.log(sum,difference,multiplication,division,"Num 1 >Num2")
+    }else{
+        console.log(sum,difference,multiplication,division,"Num 1 <Num2")
+
+    }
+
+}
+foo47(56,23)
+
+
+let  data1=prompt("enter the number");  // prompt actually returns you a string 
+let  data2=prompt("enter the number");  // prompt actually returns you a string 
+
+//console.log(typeof data);
+
+//console.log(data + data);
+//console.log(Number(data)+Number(data));
+//console.log(parseInt(data)+parseInt(data));

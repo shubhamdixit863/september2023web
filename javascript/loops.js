d1c641f70c7cd3928d4f68676b74221b649b5fa1
shubh <shubhamdixit863@gmail.com>
2023-11-10 22:04:11 +0530

    Loops examples

---
+++ javascript/loops.js
@@ -0,0 +1,96 @@
+/*function foo(a,b){
+    const sum=a+b;
+    console.log(sum); // this is for printing on the console
+
+    return sum;  // you return when you want to use the function's value outside the function
+
+}
+
+function Data(){
+    const sum=foo(8,9)
+    console.log(sum);
+}
+
+Data()
+*/
+
+//console.log(9);
+//console.log(9);
+//console.log(9);
+//console.log(9);
+//console.log(9);
+//console.log(9);
+//console.log(9);
+
+// loop is designed to repeat the process or repeat the instructions  multiple times
+
+// For loop 
+// While loop 
+// Do while loop
+
+// forEach-->higher Order js function
+
+// For loop 
+// three important things for a loop 
+// 1- from where it starts
+// 2- how it reaches from start to end
+// 3- at where it ends
+
+// My task is I want to print the console.log 9 times
+// the most basic approach that you will find in the loops is the arithmtetic
+// 1 ,2,3,4,5,6,7,8,9,10
+
+// I have to keep the track of counting 
+
+for (let i=1;i<=10;i=i+1){  // so when this condition i<=10 becomes false then our loop gets exited
+    // the logic or the code that you want to repeat will go 
+
+    console.log("9");
+    console.log("\n")  ;// its a new line character that prints extra line
+
+}
+
+// Write a function that prints all odd numbers between 1 to 100
+// write a function that prints all even numbers between 1 to 100
+// With the help of loop you have to print every even number  between 1 and 100
+// With the help of loop you have to print every odd number between 1 and 100
+
+// default parameters  --->
+
+
+function EvenNumber(){
+    for(let i=1;i<=100;i++){  // i++ -->post increment --> // pre increment ++i
+       if(i%2==0){
+        console.log(i);
+
+       }
+    }
+}
+
+function OddNumber(){
+    for(let i=1;i<=100;i++){  // i++ -->post increment --> // pre increment ++i
+       if(i%2==1){
+        console.log(i);
+
+       }
+    }
+}
+
+let i=0;
+
+for( i=1;i<=100;i++){  // i++ -->post increment --> // pre increment ++i
+    if(i%2==1){
+     console.log(i);
+
+    }
+ }
+ console.log("The value of i once teh loop is over",i);
+
+ //let i=1;  -->initialization  // -->only once // this is the first step when the loop starts
+ // i<=100  -->conditional checking part  --- // second step is checked
+ // If the condtional step is checked as true then the logic inside the loop gets executed
+
+
+ // i++  --->evaluation part (incrementing part) // after the logic is executed then the incrementing part happens
+
+ 

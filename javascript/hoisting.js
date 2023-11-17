424cc2cf90f413e8a8464963459290b40275e61b
shubh <shubhamdixit863@gmail.com>
2023-11-17 22:25:02 +0530

    hoisting

--- javascript/hoisting.js
+++ javascript/hoisting.js
@@ -1,4 +1,4 @@
-let c=9;
+let ck=9;
 
 // when the variable is actually stored in the memory
 // it is stored into three separate stages
@@ -6,3 +6,38 @@ let c=9;
 
 //  initialization
 // assignment
+// effects of hoisting 
+
+// in var 
+console.log(c);
+var c=9;
+console.log(c);
+
+//console.log(y);
+let y=7;
+//console.log(j);
+
+
+const j=88;
+
+
+
+foo()
+function foo(){
+  console.log(90);
+}
+
+f(); // whether it will work or not?
+
+// in this case let is hoisted
+// not the function
+let f=function  (){
+  console.log(8);
+}
+
+  f();
+
+/**
+ * 
+ * JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, variables, classes, or imports to the top of their scope, prior to execution of the code.
+ */

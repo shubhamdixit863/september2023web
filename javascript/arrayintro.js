4f5911e05e9a3cad731a5f386885c113f1937ca4
shubh <shubhamdixit863@gmail.com>
2023-11-19 13:40:24 +0530

    callbacks js

---
+++ javascript/arrayintro.js
@@ -0,0 +1,19 @@
+// array is a collection of data
+
+let arr=[4,5,7,8,9]; // array
+// i can iterate over an array using for loop or while loop  
+/*
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+*/
+
+//for(let i=0)  // arr.length -->it gives us total number of elements in the array
+
+for(let i=0;i<arr.length;i++){
+  console.log(arr[i]);
+}
+
+// Try printing out all the values of array using while loop

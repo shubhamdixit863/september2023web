767eaf0a18f0317bce3bcd4384bafb3285401844
shubh <shubhamdixit863@gmail.com>
2023-11-26 21:17:55 +0530

    events

--- javascript/domevents.js
+++ javascript/domevents.js
@@ -3,10 +3,27 @@ const button=document.getElementById("button");
 // event will emit no matter if you have the listener with you or not 
 // if you have the listener attached you can listen to the events and do some kind of job
 button.addEventListener("click",function(){
-    document.getElementById("heading").style.color="pink";
+    document.getElementById("heading").style.color="green";
+    if(document.getElementById("heading").style.fontFamily!="Ephesis"){
+        document.getElementById("heading").style.fontFamily="Ephesis";
+
+    }else{
+        document.getElementById("heading").style.fontFamily="";
+
+    }
+   
+
+   // document.write("hey there")
+  // document.getElementById("heading").style="color:green;font-family:Georgia, serif"
+
+
     console.log("button clicked");
 })
 
 button.addEventListener("mouseover",function(){
     console.log("mouseover heppned");
 })
+
+//button.addEventListener("")
+
+// Create a button that changes the font family of a heading when clicked 

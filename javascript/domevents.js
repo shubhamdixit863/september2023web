e16a2c65faece469e7191afe3b24a68bd9930f73
shubh <shubhamdixit863@gmail.com>
2023-11-26 21:25:05 +0530

    events

--- javascript/domevents.js
+++ javascript/domevents.js
@@ -3,7 +3,7 @@ const button=document.getElementById("button");
 // event will emit no matter if you have the listener with you or not 
 // if you have the listener attached you can listen to the events and do some kind of job
 button.addEventListener("click",function(){
-    document.getElementById("heading").style.color="green";
+   // document.getElementById("heading").style.color="green";
     if(document.getElementById("heading").style.fontFamily!="Ephesis"){
         document.getElementById("heading").style.fontFamily="Ephesis";
 
@@ -16,14 +16,27 @@ button.addEventListener("click",function(){
    // document.write("hey there")
   // document.getElementById("heading").style="color:green;font-family:Georgia, serif"
 
+// getting the input value
 
-    console.log("button clicked");
+let inputElement=document.getElementById("input");
+//console.log(inputElement.value);  // you will get the input field values---
+
+const color=inputElement.value;
+document.getElementById("heading").style.color=color;
+
+
+
 })
-
+/*
 button.addEventListener("mouseover",function(){
     console.log("mouseover heppned");
 })
-
+*/
 //button.addEventListener("")
 
 // Create a button that changes the font family of a heading when clicked 
+
+// counter app  --->try it on your own
+
+// Next task is to create a calculator -->
+// Todo list 

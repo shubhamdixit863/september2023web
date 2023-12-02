36b83f7c32171aa5244d6796477c4dfa8a18e5cb
shubh <shubhamdixit863@gmail.com>
2023-12-02 11:06:59 +0530

    js file

---
+++ javascript/calculator.js
@@ -0,0 +1,52 @@
+// this event will get fired once the dom is ready 100 percent
+
+window.onload = (event) => {
+    console.log("page is fully loaded");
+  };
+
+  // Arrow functions ,template literals
+
+window.addEventListener("load",function(){
+    let add=document.getElementById("add");
+    let subtract=document.getElementById("subtract");
+    
+    
+    let multiply=document.getElementById("multiply");
+    let one=document.getElementById("one");
+    let two=document.getElementById("two");
+    let output=document.getElementById("output");
+    
+    
+
+   
+    
+    
+    add.addEventListener("click",function(){
+         let onevalue=one.value;
+         let twovalue=two.value;
+    
+         // parseInt(1,2,3,) ,parseFloat  (7.9,8.9)
+         let sum=Number(onevalue)+Number(twovalue);
+         output.value=sum;
+    
+    })
+    
+    multiply.addEventListener("click",function(){
+        let onevalue=one.value;
+         let twovalue=two.value;
+    
+         // parseInt(1,2,3,) ,parseFloat  (7.9,8.9)
+         let product=Number(onevalue)*Number(twovalue);
+         output.value=product;
+        
+    })
+    subtract.addEventListener("click",function(){
+        let onevalue=one.value;
+         let twovalue=two.value;
+    
+         // parseInt(1,2,3,) ,parseFloat  (7.9,8.9)
+         let difference=Number(onevalue)-Number(twovalue);
+         output.value=difference;
+    })
+})
+

8f2390ef02d2d4a597b92f706a22d388a4f19f40
shubh <shubhamdixit863@gmail.com>
2023-12-23 21:22:21 +0530

    async api

--- javascript/async.js
+++ javascript/async.js
@@ -24,3 +24,68 @@ console.log(0);
 // reaching office 10 min
 
 // 8 am --->8:15 --->8:20 -->8:22 -->reach office -->8:32
+
+
+
+/*
+// map ,filter ,forEach  --->higher order function
+
+
+function foo(){
+  setTimeout(function(){
+      bar()
+  },1000)
+
+
+  console.log("foo")
+}
+
+
+function bar(){
+  console.log("bar");
+}
+
+bar();  // sync 
+
+setTimeout(function(){
+    foo();
+},1000)
+
+
+console.log("hello world");  // sync
+bar
+hello world
+foo
+bar
+*/
+/*
+
+const interval=setInterval(function(){
+  console.log(8);
+},1000)
+
+
+setTimeout(()=>{
+  clearInterval(interval);  // it just finishes the setInterval
+},5000)
+*/
+
+function foo(){
+    setInterval(function(){
+      console.log("hey there");
+    },2000)
+  
+    console.log("foo")
+  }
+  
+  
+  foo();
+  
+  setTimeout(()=>{
+    console.log("hey i will be in between .....")
+  },5000)
+  
+  
+  console.log(8);
+  
+  //hey there ,8,hey there ,hey there,hey there,hey there,hey i will be in between .....,

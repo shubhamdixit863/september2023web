808c7be7e7d4cf4af76e22e9eb5877cc0e6d7822
shubh <shubhamdixit863@gmail.com>
2024-01-14 20:41:52 +0530

    space complexity

--- dsa/algo.js
+++ dsa/algo.js
@@ -62,7 +62,7 @@ Printing([1,2,3,3,7,8,9,19,1,8,123,67,890]);
 
 // Time complexity of algo is determined by --->number of operations performed by the cpu 
 //--->which is dependent on size of input
-
+/*
 // determine numbeer of operations taken by the foo function
 function foo(arr){
     let i=0; // 1 
@@ -76,5 +76,112 @@ function foo(arr){
 return arr2;  // 1 operation
 
 }
+*/
 
-foo();   // 1+1 +1+n+n+1 =4+5n operations where n is the length of array
+//foo();   // 1+1 +1+n+n+1 =4+5n operations where n is the length of array
+
+
+// 
+let obj={};
+let r=[2,3,4,4];
+let objArr=[{name:"doma"},{name:"john"}];
+console.log(
+
+    objArr.filter(ele=> ele.name=="john")
+)
+
+
+
+// object destructring and array destructing  assignment
+let obj1={age:22,city:"Delhi"}
+
+const {age,city}=obj1;
+console.log("Name of an obj",age,city); // obj1.age ,obj1.city
+
+// destructring assignment in an array 
+
+let arr=[1,2,3,4];
+const [a,b,c]=arr;
+
+console.log(a,b,c);
+
+// react 
+
+ // const[state,setState]= useState("initialState");
+
+ ///
+
+ function algo1(c){
+ 
+    console.log(c);  //1
+    let d=c*2;  //2 
+    console.log(d);  //1
+   return ; //1
+    //1
+ }
+ algo1(8);  // 5 operations
+
+ algo1(80);  // 5 operations
+ algo1(2.5);
+
+ //algo1(8000000,[1,2,3,3,4],890);  // 5 operations
+
+
+
+
+// O(1);
+
+
+ // O(5) =5
+ // O(1)=1
+ // O(1)
+
+ // for this algorithm the number of operations are constant
+
+ function algo2(arr){
+    let arr2=[]  // 1
+    let c= arr.forEach(Ele=>{
+         arr2.push(Ele*Ele);  // 2n
+    }) //1
+return arr2; //1
+ }
+
+ algo2([2,3,4,5,6])  // 13  operations
+
+ algo2([]) // length n ---> 2n+3 -if the value of n is 5 -->13
+ // algo([20 elems])  --->  2*20+3= 43
+ // here n is the size of input 
+
+
+ // ---->Moving toward Big O;
+
+ // big O is the general formular in which the number of operations performed by 
+ // the algo can be resprsented
+ // O(n) is the representation
+ // O(n)=2n+3;
+ // O(100000)=200000+3
+
+ // O(n)=2n
+ // O(n) =n
+
+ // Time complexity
+ //  O(n)
+
+
+ // And generally what we say that time complexity if of order N
+ // O(n2)
+ // if we have complexity of O(n2) we say time complexity if of order N square
+
+ // O(1)  -->constant time
+ // O(N) -->good time complexity
+ // O(N2)
+ // O(N3)
+ // O(logn)
+ // O(nlogn);
+
+
+ function footrt(arr){
+
+ }
+ footrt([7,8,9,0])
+
---
+++ dsa/spacecomplexity.js
@@ -0,0 +1,96 @@
+function foo(num){
+    let arr=[];
+    for (let i=0;i<num;i++){
+        arr.push(i*8);
+    }
+
+}
+let arr=[1,2,3,4,5,6];
+
+foo(100)
+
+// The space complexity of your algorithm is actually determined by the
+// amount of memory it occupies during run time
+
+// O(N);
+
+function foo(arr){
+    let arr2=[];
+    for(let i=0;i<arr.length;i++){
+      arr2.push(arr[i]*9);
+    }
+
+return arr2;
+}
+
+// the space complexity here is O(N);
+// quadratic space complexity
+
+function foo(arr){
+    let arr2=[];
+    for(let i=0;i<arr.length;i++){
+      arr2.push([i,arr[0]]);
+    }
+
+return arr2;
+}
+
+// quadratic space complexity or O(N2)
+
+function createMatrix(n){
+    let matrix=[];
+    for (let i=0;i<n;i++){
+        matrix[i]=[];
+        for (let j=0;j<n;j++){
+            matrix[i][j]=i+j;
+        }
+    }
+return matrix;
+}
+
+
+
+// space complexity would be linear
+function createMatrix(arr){
+    let obj={};
+    for(let i=0;i<arr.length;i++){
+        obj[i]=arr[i];
+    }
+  
+}
+
+
+
+
+// cubic space complexity --- three dimensional array 
+
+// ON(4) --->4 dimensional array
+
+
+/// 
+
+
+var twoSum = function(nums, target) {
+    
+    const map=new Map();
+for(let i=0;i<nums.length;i++){
+    const complement=target-nums[i];
+    if(map.get(complement)!==undefined){ // if you are adding anything to map or getting anything O(1)
+        return [map.get(complement),i]
+    }
+   // map[nums[i]]=i;  // O(1)
+   map.set(nums[i],i)
+
+}  
+
+};
+// O(1);
+function foo2(n){
+    let j=0;
+    for(let i=0;i<n;i++){
+       let k=i;
+    }
+    //  ---  you should have n separate space occupied
+} 
+
+foo2(8888)

91d119690d6b264bdca84d9f9071f494aadc2bee
shubh <shubhamdixit863@gmail.com>
2024-01-19 21:42:54 +0530

    array swap

--- dsa/selectionsort.js
+++ dsa/selectionsort.js
@@ -1,23 +1,34 @@
-function selectionSort(arr){
+let swapCount=0;
+
+function selectionsort(arr){
 
     for(let i=0;i<arr.length;i++){
         let min=i;
+        let swap = false;
         for(let j=i+1;j<arr.length;j++){
           // here we will change the value of min
           if(arr[min]>arr[j]){
             min=j;
+            swap = true;
           }
 
         }
 
         // we will make a swap here
-        let temp=arr[i];
-        arr[i]=arr[min];
-        arr[min]=temp;
+        if(swap){
+            let temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+            swapCount++;
+        }
+       
 
     }
 
 return arr;
 }
 
-console.log(selectionSort([78,2,12,90,0,2]))
+console.log(selectionsort([7, 5, 2, 6, 3]));  //3 
+console.log(swapCount);
+
+// convert selection sort above to use while loop

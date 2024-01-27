a3025dbc5f71a89a4befc2ae49407955f1a1c3c4
shubh <shubhamdixit863@gmail.com>
2024-01-27 21:34:56 +0530

    leet code

--- dsa/stack.js
+++ dsa/stack.js
@@ -41,3 +41,49 @@ console.log(stack.peek());
 console.log(stack);
 console.log(stack.pop());
 console.log(stack);
+
+
+
+/**
+ * Leet code problem 
+
+*/
+
+
+//let stack=[];
+
+var isValid = function(str) {
+  let openingBrackets=["(","{","["];
+   let closingBrackets=[")","}","]"];
+
+   for (let i=0;i<str.length;i++){
+       if(openingBrackets.includes(str[i])){
+          stack.push(str[i]);
+       }else{
+
+           //  we have to check whether the stack has its opening bracket or not
+           // we will pop the stack
+          let ele= stack.pop()
+        // console.log(ele,str[i]);
+          let indexofOpening=openingBrackets.indexOf(ele);
+          let indexOfClosing=closingBrackets.indexOf(str[i]);
+        // console.log(indexOfClosing,indexofOpening);
+          if (indexofOpening!=indexOfClosing){
+              return false
+          }
+      
+
+       }
+   }
+  if(stack.length==0){
+      return true;
+  }
+
+   return false;
+
+};
+
+// ([{())}])
+// ([)(]) // invalid string
+
+console.log(isValid("{[]}"))
